25421831db1718c8f4ec376e312cddb65a6c78f0
“cjhgit” <1418503647@qq.com>
2022-08-20 17:16:28 +0800

    查看索引

--- src/views/db-manager/table-detail/table-detail.tsx
+++ src/views/db-manager/table-detail/table-detail.tsx
@@ -1,11 +1,16 @@
-import { Table } from 'antd';
+import { Table, Tabs } from 'antd';
+import React from 'react';
 import { VFC, useRef, useState, useEffect } from 'react';
 import { request } from '../utils/http';
 import styles from './table-detail.module.less';
+import _ from 'lodash';
+console.log('lodash', _)
+const { TabPane } = Tabs
 
 export function TableDetail({ config, dbName, tableName }) {
 
     const [tableInfo, setTableInfo] = useState([])
+    const [indexes, setIndexes] = useState([])
     const [modelVisible, setModalVisible] = useState(false)
     const [modelCode, setModalCode] = useState('')
     const [fields, setFields] = useState([])
@@ -36,17 +41,72 @@ export function TableDetail({ config, dbName, tableName }) {
             dataIndex: 'COLUMN_COMMENT',
         },
     ]
+    const indexColumns = [
+        {
+            title: '索引名',
+            dataIndex: 'name',
+        },
+        {
+            title: '包含列',
+            dataIndex: 'columns',
+        },
+        // {
+        //     title: 'COLUMN_NAME',
+        //     dataIndex: 'COLUMN_NAME',
+        // },
+        {
+            title: '备注',
+            dataIndex: 'comment',
+        },
+        {
+            title: '类型',
+            dataIndex: 'type',
+        },
+        {
+            title: '不唯一',
+            dataIndex: 'NON_UNIQUE',
+        },
+        
+        
+        
+    ]
     async function loadTableInfo() {
         if (dbName && tableName) {
-            let res = await request.get(`${config.host}/mysql/databases/${dbName}/tables/${tableName}/columns`, {
+            let res = await request.get(`${config.host}/mysql/databases/${dbName}/tables/${tableName}/all`, {
                 noMessage: true,
             })
             console.log('loadTableInfo', res)
             if (res.status == 200) {
-                setTableInfo(res.data)
-
-
-
+                setTableInfo(res.data.columns)
+                
+                const groupMap = _.groupBy(res.data.indexes, 'INDEX_NAME')
+                console.log('groups2', groupMap)
+                const indexes = []
+                for (let key in groupMap) {
+                    const item0 = groupMap[key][0]
+                    const columns = groupMap[key]
+                    indexes.push({
+                        name: item0.INDEX_NAME,
+                        comment: item0.INDEX_COMMENT,
+                        type: item0.INDEX_TYPE,
+                        NON_UNIQUE: item0.NON_UNIQUE,
+                        columns: columns
+                            .sort((a, b) => {
+                                return a.SEQ_IN_INDEX - b.SEQ_IN_INDEX
+                            })
+                            .map(item => item.COLUMN_NAME)
+                            .join(', ')
+                        // {
+                        //     title: 'SEQ_IN_INDEX',
+                        //     dataIndex: 'SEQ_IN_INDEX',
+                        // },
+                        // {
+                        //     title: 'COLUMN_NAME',
+                        //     dataIndex: 'COLUMN_NAME',
+                        // },
+                    })
+                }
+                setIndexes(indexes)
             }
         }
     }
@@ -57,13 +117,29 @@ export function TableDetail({ config, dbName, tableName }) {
     
 	return (
         <div className={styles.detailBox}>
-            <Table
-                columns={columns}
-                dataSource={tableInfo}
-                bordered
-                pagination={false}
-                size="small"
-            />
+            <Tabs
+                tabPosition="left"
+                type="card"
+            >
+                <TabPane tab="列信息" key="columns">
+                    <Table
+                        columns={columns}
+                        dataSource={tableInfo}
+                        bordered
+                        pagination={false}
+                        size="small"
+                    />
+                </TabPane>
+                <TabPane tab="索引信息" key="index">
+                    <Table
+                        columns={indexColumns}
+                        dataSource={indexes}
+                        bordered
+                        pagination={false}
+                        size="small"
+                    />
+                </TabPane>
+            </Tabs>
         </div>
     )
 }
