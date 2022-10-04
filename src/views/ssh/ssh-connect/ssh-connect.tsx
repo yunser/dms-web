8e3b1b11ff703370c6f04c38d32f2f8ea29b3ccd
cjhgit <1418503647@qq.com>
2022-10-04 20:19:46 +0800

    新增 - 编辑连接

--- src/views/ssh/ssh-connect/ssh-connect.tsx
+++ src/views/ssh/ssh-connect/ssh-connect.tsx
@@ -1,4 +1,4 @@
-import { Button, Descriptions, Dropdown, Empty, Input, Menu, message, Modal, Popover, Space, Table, Tabs, Tag } from 'antd';
+import { Button, Descriptions, Dropdown, Empty, Form, Input, InputNumber, Menu, message, Modal, Popover, Space, Table, Tabs, Tag } from 'antd';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import styles from './ssh-connect.module.less';
 import _ from 'lodash';
@@ -6,7 +6,7 @@ import classNames from 'classnames'
 // console.log('lodash', _)
 import { useTranslation } from 'react-i18next';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import { DownloadOutlined, EllipsisOutlined, PlusOutlined, ReloadOutlined } from '@ant-design/icons';
+import { DownloadOutlined, EllipsisOutlined, EyeInvisibleOutlined, EyeOutlined, PlusOutlined, ReloadOutlined } from '@ant-design/icons';
 import saveAs from 'file-saver';
 import { useEventEmitter } from 'ahooks';
 // import { GitProject } from '../git-project';
@@ -17,10 +17,36 @@ import { FullCenterBox } from '@/views/db-manager/redis-client';
 import { SshDetail } from '../ssh-home';
 // import { saveAs } from 'file-saver'
 import { FileList } from '../../file/file-list'
+import storage from '@/utils/storage';
+import { uid } from 'uid';
+
+function InputPassword(props) {
+    const [visible, setVisible] = useState(false)
+    return (
+        <Input
+            {...props}
+            type={visible ? 'text' : 'password'}
+            addonAfter={
+                <div>
+                    <IconButton
+                        size="small"
+                        onClick={() => {
+                            setVisible(!visible)
+                        }}
+                    >
+                        {visible ? <EyeOutlined /> : <EyeInvisibleOutlined /> }
+                    </IconButton>
+                </div>
+            }
+        />
+    )
+}
 
 export function SshConnect() {
     // const { defaultJson = '' } = data
     const { t } = useTranslation()
+    const [modalVisible, setModalVisible] = useState(false)
+    const [modalItem, setModalItem] = useState(false)
     const [curItem, setCurItem] = useState(null)
     const [view, setView] = useState('list')
     const [keyword, setKeyword] = useState('')
@@ -129,54 +155,15 @@ export function SshConnect() {
                                 >
                                     <ReloadOutlined />
                                 </IconButton>
-                                {/* <Dropdown
-                                    trigger={['click']}
-                                    overlay={
-                                        <Menu
-                                            items={[
-                                                {
-                                                    label: t('git.clone_from_url'),
-                                                    key: 'clone_from_url',
-                                                },
-                                                {
-                                                    label: t('git.add_exists_local_repository'),
-                                                    key: 'add_exists',
-                                                },
-                                                {
-                                                    label: t('git.create_local_repository'),
-                                                    key: 'create_git',
-                                                },
-                                            ]}
-                                            onClick={({ key }) => {
-                                                if (key == 'add_exists') {
-                                                    setProjectModalVisible(true)
-                                                    setProjectItem(null)
-                                                    setCreateType('exists')
-                                                }
-                                                else if (key == 'clone_from_url') {
-                                                    setCloneModalVisible(true)
-                                                    setProjectItem(null)
-                                                    setCreateType('clone')
-                                                }
-                                                else if (key == 'create_git') {
-                                                    setProjectModalVisible(true)
-                                                    setProjectItem(null)
-                                                    setCreateType('init')
-                                                }
-                                            }}
-                                        />
-                                    }
+                                <IconButton
+                                    // tooltip={t('add')}
+                                    className={styles.refresh}
+                                    // onClick={() => {
+                                    //     setProjectModalVisible(true)
+                                    // }}
                                 >
-                                    <IconButton
-                                        // tooltip={t('add')}
-                                        className={styles.refresh}
-                                        // onClick={() => {
-                                        //     setProjectModalVisible(true)
-                                        // }}
-                                    >
-                                        <PlusOutlined />
-                                    </IconButton>
-                                </Dropdown> */}
+                                    <PlusOutlined />
+                                </IconButton>
                             </Space>
                         </div>
                         {/* <div>
@@ -232,6 +219,16 @@ export function SshConnect() {
                                                     >
                                                         SFTP
                                                     </Button>
+                                                    <Button
+                                                        size="small"
+                                                        onClick={() => {
+                                                            setModalVisible(true)
+                                                            setModalItem((item))
+                                                        }}
+                                                    >
+                                                        {t('edit')}
+                                                    </Button>
+                                                    
                                                     {/* {!!item.changes && item.changes > 0 &&
                                                         <div className={styles.branch}>
                                                             <div className={styles.changes}>{item.changes}</div>
@@ -337,6 +334,223 @@ export function SshConnect() {
                     }}
                 />
             } */}
+            {modalVisible &&
+                <DatabaseModal
+                    item={modalItem}
+                    config={config}
+                    onCancel={() => {
+                        setModalVisible(false)
+                    }}
+                    onSuccess={() => {
+                        setModalVisible(false)
+                        loadList()
+                    }}
+                />
+            }
         </div>
     )
+}
+
+
+
+function DatabaseModal({ config, onCancel, item, onSuccess, onConnnect, }) {
+    const { t } = useTranslation()
+
+    const editType = item ? 'update' : 'create'
+    const [loading, setLoading] = useState(false)
+    const [form] = Form.useForm()
+//     const [code, setCode] = useState(`{
+//     "host": "",
+//     "user": "",
+//     "password": ""
+// }`)
+
+    
+
+    useEffect(() => {
+        if (item) {
+            form.setFieldsValue({
+                ...item,
+                defaultDatabase: item.defaultDatabase || 0,
+            })
+        }
+        else {
+            form.setFieldsValue({
+                name: '',
+                host: '',
+                port: null,
+                password: '',
+                defaultDatabase: null,
+                userName: '',
+            })
+        }
+    }, [item])
+
+    async function handleOk() {
+        const values = await form.validateFields()
+        // setLoading(true)
+        let _connections
+        if (editType == 'create') {
+            const connections = storage.get('redis-connections', [])
+            if (connections.length) {
+                _connections = connections
+            }
+            else {
+                _connections = []
+            }
+            _connections.unshift({
+                id: uid(32),
+                name: values.name || t('unnamed'),
+                host: values.host || 'localhost',
+                port: values.port || 6379,
+                // user: values.user,
+                password: values.password,
+                userName: values.userName,
+                defaultDatabase: values.defaultDatabase || 0,
+                // db: values.db,
+            })
+        }
+        else {
+            let res = await request.post(`${config.host}/ssh/connection/edit`, {
+                id: item.id,
+                data: {
+                    name: values.name || t('unnamed'),
+                    host: values.host || 'localhost',
+                    port: values.port || 6379,
+                    password: values.password,
+                    username: values.username,
+                }
+            }, {
+                // noMessage: true,
+            })
+            // console.log('res', res)
+            if (res.success) {
+                onSuccess && onSuccess()
+            }
+        }
+    }
+
+    async function handleTestConnection() {
+        const values = await form.validateFields()
+        setLoading(true)
+        const reqData = {
+            host: values.host || 'localhost',
+            port: values.port || 6379,
+            // user: values.user,
+            password: values.password,
+            userName: values.userName,
+            db: values.defaultDatabase || 0,
+            test: true,
+            // remember: values.remember,
+        }
+        let ret = await request.post(`${config.host}/redis/connect`, reqData)
+        // console.log('ret', ret)
+        if (ret.success) {
+            message.success(t('success'))
+        }
+        setLoading(false)
+    }
+
+    return (
+        <Modal
+            title={editType == 'create' ? t('connection_create') : t('connection_update')}
+            visible={true}
+            maskClosable={false}
+            onCancel={onCancel}
+            // onOk={async () => {
+                
+            // }}
+            footer={(
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <div></div>
+                    {/* <Button key="back"
+                        loading={loading}
+                        disabled={loading}
+                        onClick={handleTestConnection}
+                    >
+                        {t('test_connection')}
+                    </Button> */}
+                    <Space>
+                        <Button
+                            // key="submit"
+                            // type="primary"
+                            disabled={loading}
+                            onClick={onCancel}
+                        >
+                            {t('cancel')}
+                        </Button>
+                        <Button
+                            type="primary"
+                            disabled={loading}
+                            onClick={handleOk}
+                        >
+                            {t('ok')}
+                        </Button>
+                    </Space>
+                </div>
+            )}
+        >
+            <Form
+                form={form}
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 18 }}
+                // layout={{
+                //     labelCol: { span: 0 },
+                //     wrapperCol: { span: 24 },
+                // }}
+            >
+                <Form.Item
+                    name="name"
+                    label={t('title')}
+                    // rules={[ { required: true, }, ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    name="host"
+                    label={t('host')}
+                    // rules={[ { required: true, }, ]}
+                >
+                    <Input
+                        placeholder="localhost"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="port"
+                    label={t('port')}
+                    // rules={[{ required: true, },]}
+                >
+                    <InputNumber
+                        placeholder="6379"
+                    />
+                </Form.Item>
+                {/* <Form.Item
+                    name="user"
+                    label="User"
+                    rules={[{ required: true, },]}
+                >
+                    <Input />
+                </Form.Item> */}
+                <Form.Item
+                    name="username"
+                    label={t('user_name')}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    label={t('password')}
+                    rules={[{ required: true, },]}
+                >
+                    <InputPassword />
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
 }
