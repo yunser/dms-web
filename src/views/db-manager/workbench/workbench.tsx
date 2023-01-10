9ea88ae4c3ddf8ff5066bff544ad16ca7f4fbc64
cjhgit <1418503647@qq.com>
2023-01-11 02:30:18 +0800

    新增 - 工作台推荐可关闭

--- src/views/db-manager/workbench/workbench.tsx
+++ src/views/db-manager/workbench/workbench.tsx
@@ -6,6 +6,8 @@ import classNames from 'classnames'
 import { useTranslation } from 'react-i18next';
 import storage from '@/utils/storage'
 import { useInterval } from 'ahooks';
+import { IconButton } from '../icon-button';
+import { EyeInvisibleOutlined } from '@ant-design/icons';
 
 const prodLogos = {
     'mysql': 'mysql.svg',
@@ -22,7 +24,9 @@ export function Workbench({ config, onCommand }) {
     const { t } = useTranslation()
 
     const [historyApps, setHistoryApps] = useState([])
-
+    const [recommendVisible, setRecommendVisible] = useState(() => {
+        return storage.get('recommendVisible', true)
+    })
     async function loadHistoryApps() {
         const historyApps = storage.get('historyApps', [])        
         setHistoryApps(historyApps)
@@ -78,26 +82,41 @@ export function Workbench({ config, onCommand }) {
                     </div>
                 </div>
             }
-            <div className={styles.section}>
-                <div className={styles.title}>{t('recommend')}</div>
-                <div className={styles.list}>
-                    {commands.map(item => {
-                        return (
-                            <div className={styles.item}
-                                key={item.command}
-                                onClick={() => {
-                                    onCommand && onCommand(item.command)
-                                }}
-                            >
-                                <div className={styles.name}>{item.name}</div>
-                                {!!prodLogos[item.command] &&
-                                    <img className={styles.logo} src={`/prod-logo/${prodLogos[item.command]}`} />
-                                }
-                            </div>
-                        )
-                    })}
+            {recommendVisible &&
+                <div className={styles.section}>
+                    <div className={styles.title}>
+                        <div>{t('recommend')}</div>
+                        <IconButton
+                            className={styles.icon}
+                            tooltip={t('hide')}
+                            onClick={() => {
+                                // setAllList(allList.filter(_item => _item.id != item.id))
+                                setRecommendVisible(!recommendVisible)
+                                storage.set('recommendVisible', !recommendVisible)
+                            }}
+                        >
+                            <EyeInvisibleOutlined />
+                        </IconButton>
+                    </div>
+                    <div className={styles.list}>
+                        {commands.map(item => {
+                            return (
+                                <div className={styles.item}
+                                    key={item.command}
+                                    onClick={() => {
+                                        onCommand && onCommand(item.command)
+                                    }}
+                                >
+                                    <div className={styles.name}>{item.name}</div>
+                                    {!!prodLogos[item.command] &&
+                                        <img className={styles.logo} src={`/prod-logo/${prodLogos[item.command]}`} />
+                                    }
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
