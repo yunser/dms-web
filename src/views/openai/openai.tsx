8900e2988d7b4f1df81f3bc5630172eef52d343f
cjhgit <1418503647@qq.com>
2023-03-04 15:17:40 +0800

    修改 - AI 地址从接口获取

--- src/views/openai/openai.tsx
+++ src/views/openai/openai.tsx
@@ -1,14 +1,39 @@
+import { getGlobalConfig } from '@/config'
+import { Spin } from 'antd'
 import React, { useEffect, useState } from 'react'
+import { request } from '../db-manager/utils/http'
 import { AiChat } from './chat'
 
-const host = 'https://xxx.com'
-
 export function OpenAiApp() {
+    const config = getGlobalConfig()
+    const [loading, setLoading] = useState(false)
+    const [url, setUrl] = useState('')
+
+    async function loadData() {
+        let res = await request.post(`${config.host}/openai/info`, {
+        })
+        if (res.success) {
+            setUrl(res.data.url)
+            setLoading(false)
+        }
+        else {
+            // setErr('Connect rrror')
+        }
+    }
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
     return (
         <div>
-            <AiChat
-                host={host}
-            />
+            {loading ?
+                <Spin />
+            :
+                <AiChat
+                    host={url}
+                />
+            }
         </div>
     )
 }
